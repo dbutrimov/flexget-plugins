b3f4258c194218e8f4b56e2701d3f82354d92fdc
Dmitry Butrimov <d.butrimov@gmail.com>
2016-10-16 22:18:39 +0300

    search plugin for newstudio

--- newstudio.py
+++ newstudio.py
@@ -9,6 +9,7 @@ import urllib
 import logging
 
 from flexget import plugin
+from flexget.entry import Entry
 from flexget.event import event
 from flexget.utils import requests
 
@@ -17,8 +18,19 @@ log = logging.getLogger('newstudio')
 viewtopic_url_regexp = re.compile(r'^https?://(?:www\.)?newstudio\.tv/viewtopic\.php\?t=(\d+).*$', flags=re.IGNORECASE)
 download_url_regexp = re.compile(r'^(?:.*)download.php\?id=(\d+)$', flags=re.IGNORECASE)
 
+ep_regexp = re.compile(r"\([Сс]езон\s+(\d+)\W+[Cс]ерия\s+(\d+)\)", flags=re.IGNORECASE)
+
 host_prefix = 'http://newstudio.tv/'
 host_regexp = re.compile(r'^https?://(?:www\.)?newstudio\.tv.*$', flags=re.IGNORECASE)
+
+
+class NewStudioShow(object):
+    titles = []
+    url = ''
+
+    def __init__(self, titles, url):
+        self.titles = titles
+        self.url = url
 
 
 class NewStudioUrlRewrite(object):
@@ -41,7 +53,7 @@ class NewStudioUrlRewrite(object):
             log.error(reject_reason)
             entry.reject(reject_reason)
             return False
-        viewtopic_html = viewtopic_response.text
+        viewtopic_html = viewtopic_response.content
 
         viewtopic_soup = BeautifulSoup(viewtopic_html, 'html.parser')
         download_node = viewtopic_soup.find('a', href=download_url_regexp)
@@ -56,7 +68,123 @@ class NewStudioUrlRewrite(object):
         entry.reject(reject_reason)
         return False
 
+    def search(self, task, entry, config=None):
+        entries = set()
+
+        serials_url = 'http://newstudio.tv/'
+
+        log.debug("Fetching serials page `{0}`...".format(serials_url))
+
+        try:
+            serials_response = task.requests.get(serials_url)
+        except requests.RequestException as e:
+            log.error("Error while fetching page: {0}".format(e))
+            return None
+        serials_html = serials_response.content
+
+        log.debug("Parsing serials page `{0}`...".format(serials_url))
+
+        serials_tree = BeautifulSoup(serials_html, 'html.parser')
+        accordion_node = serials_tree.find('div', class_='accordion', id='serialist')
+        if not accordion_node:
+            log.error("Error while parsing serials page: node <div class=`accordion` id=`serialist`> are not found")
+            return None
+
+        shows = set()
+        inner_nodes = accordion_node.find_all('div', class_='accordion-inner')
+        for inner_node in inner_nodes:
+            link_nodes = inner_node.find_all('a')
+            for link_node in link_nodes:
+                title = link_node.text
+                viewforum_link = link_node.get('href')
+                viewforum_link = self.add_host_if_need(viewforum_link)
+
+                show = NewStudioShow([title], viewforum_link)
+                shows.add(show)
+
+        log.debug("{0:d} shows are found".format(len(shows)))
+
+        search_regexp = re.compile('^(.*?)\s*s(\d+?)e(\d+?)$', flags=re.IGNORECASE)
+        for search_string in entry.get('search_strings', [entry['title']]):
+            search_match = search_regexp.search(search_string)
+            if not search_match:
+                continue
+
+            search_title = search_match.group(1)
+            search_season = int(search_match.group(2))
+            search_episode = int(search_match.group(3))
+
+            # log.debug('search_title: {0}; search_season: {1}; search_episode: {2}'.format(
+            #     search_title, search_season, search_episode))
+
+            for show in shows:
+                if search_title not in show.titles:
+                    continue
+
+                show_pages = [show.url]
+                page_index = 0
+                while page_index < len(show_pages):
+                    current_page_index = page_index
+                    page_index += 1
+
+                    page_url = show_pages[current_page_index]
+                    try:
+                        viewforum_response = task.requests.get(page_url)
+                    except requests.RequestException as e:
+                        log.error("Error while fetching page: {0}".format(e))
+                        continue
+                    viewforum_html = viewforum_response.content
+
+                    viewforum_tree = BeautifulSoup(viewforum_html, 'html.parser')
+
+                    if current_page_index < 1:
+                        pagination_node = viewforum_tree.find('div', class_=re.compile(r'pagination.*', flags=re.IGNORECASE))
+                        if pagination_node:
+                            pagination_link_nodes = pagination_node.find_all('a')
+                            for pagination_link_node in pagination_link_nodes:
+                                page_number_text = pagination_link_node.text
+                                try:
+                                    page_number = int(page_number_text)
+                                except Exception:
+                                    continue
+                                page_link = pagination_link_node.get('href')
+                                page_link = self.add_host_if_need(page_link)
+                                show_pages.append(page_link)
+
+                    accordion_node = viewforum_tree.find('div', class_='accordion-inner')
+                    if not accordion_node:
+                        continue
+
+                    row_nodes = accordion_node.find_all('div', class_='row-fluid')
+                    viewtopic_link_regexp = re.compile(r'.*/viewtopic\.php\?t=(\d+).*', flags=re.IGNORECASE)
+                    for row_node in row_nodes:
+                        link_node = row_node.find('a', class_='torTopic tt-text', href=viewtopic_link_regexp)
+                        if not link_node:
+                            continue
+
+                        title = link_node.text
+                        ep_match = ep_regexp.search(title)
+                        if not ep_match:
+                            continue
+
+                        season = int(ep_match.group(1))
+                        episode = int(ep_match.group(2))
+                        # log.debug("{0} (s{1:02d}e{2:02d})".format(title, season, episode))
+                        if season != search_season or episode != search_episode:
+                            continue
+
+                        torrent_url = link_node.get('href')
+                        torrent_url = self.add_host_if_need(torrent_url)
+
+                        entry = Entry()
+                        entry['title'] = "{0} (s{1:02d}e{2:02d})".format(search_title, season, episode)
+                        entry['url'] = torrent_url
+
+                        entries.add(entry)
+
+        return entries
+
 
 @event('plugin.register')
 def register_plugin():
-    plugin.register(NewStudioUrlRewrite, 'newstudio', groups=['urlrewriter'], api_ver=2)
+    plugin.register(NewStudioUrlRewrite, 'newstudio', groups=['urlrewriter', 'search'], api_ver=2)
