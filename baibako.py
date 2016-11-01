4f8fb616613b64b6cdcff5b740f344d24f1e0298
Dmitry Butrimov <d.butrimov@gmail.com>
2016-11-01 23:58:45 +0300

    baibako as rewrite/search plugin

--- baibako.py
+++ baibako.py
@@ -4,8 +4,26 @@ from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
 import re
 from bs4 import BeautifulSoup
+import urllib
 
-import requests
+import logging
+
+from flexget import plugin
+from flexget.entry import Entry
+from flexget.event import event
+from flexget.utils import requests
+
+log = logging.getLogger('baibako')
+
+host_prefix = 'http://baibako.tv/'
+host_regexp = re.compile(r'^https?://(?:www\.)?baibako\.tv.*$', flags=re.IGNORECASE)
+
+details_url_regexp = re.compile(r'^https?://(?:www\.)?baibako\.tv/details\.php\?id=(\d+).*$', flags=re.IGNORECASE)
+
+table_class_regexp = re.compile(r'table.*', flags=re.IGNORECASE)
+
+episode_title_regexp = re.compile(r'^([^/]*?)\s*/\s*([^/]*?)\s*/\s*s(\d+)e(\d+)\s*/\s*([^/]*?)\s*(?:(?:/.*)|$)',
+                                  flags=re.IGNORECASE)
 
 
 class BaibakoShow(object):
@@ -17,73 +35,153 @@ class BaibakoShow(object):
         self.url = url
 
 
-headers = {'cookie': 'PHPSESSID=updjhuc3mlac37ejge5it5kk51; uid=91346; pass=065333565042aca10d1e0aeccc477e20'}
+class BaibakoUrlRewrite(object):
+    """
+        BaibaKo urlrewriter.
 
-try:
-    serials_response = requests.get('http://baibako.tv/serials.php', headers=headers)
-except requests.RequestException as e:
-    print(e)
-serials_html = serials_response.text
+        Example::
 
-table_class_regexp = re.compile(r'table.*', flags=re.IGNORECASE)
-episode_title_regexp = re.compile(r'^([^/]*?)\s*/([^/]*?)\s*/\s*s(\d+)e(\d+)\s*/\s*([^/]*?)\s*/.*$', flags=re.IGNORECASE)
+          baibako:
+            serial_tab: 'hd720'
+        """
 
-shows = set()
+    config = {}
 
-serials_tree = BeautifulSoup(serials_html, 'html.parser')
-table_node = serials_tree.find('table', class_=table_class_regexp)
-if table_node:
-    link_nodes = table_node.find_all('a')
-    for link_node in link_nodes:
-        title = link_node.text
-        href = 'http://baibako.tv/' + link_node.get('href')
-        # print("{0} - {1}".format(title.encode('windows-1251', 'replace'), href))
+    schema = {
+        'type': 'object',
+        'properties': {
+            'serial_tab': {'type': 'string'}
+        },
+        'additionalProperties': False
+    }
 
-        show = BaibakoShow([title], href)
-        shows.add(show)
+    def add_host_if_need(self, url):
+        if not host_regexp.match(url):
+            url = urllib.parse.urljoin(host_prefix, url)
+        return url
 
-print("{0} show(s) found!".format(len(shows)))
+    def on_task_start(self, task, config):
+        if not isinstance(config, dict):
+            log.verbose("Config was not determined - use default.")
+        else:
+            self.config = config
 
-serial_tab = 'hd720'
-search_title = "Скорпион"  # "11.22.63"
-print(search_title)
-for show in shows:
-    if search_title not in show.titles:
-        continue
+    def url_rewritable(self, task, entry):
+        url = entry['url']
+        return details_url_regexp.match(url)
 
-    serial_url = show.url + '&tab=' + serial_tab
-    print(serial_url)
-    try:
-        serial_response = requests.get(serial_url, headers=headers)
-    except requests.RequestException as e:
-        print(e)
-    serial_html = serial_response.text
+    def url_rewrite(self, task, entry):
+        url = entry['url']
+        url_match = details_url_regexp.search(url)
+        if not url_match:
+            reject_reason = 'Url don''t matched'
+            log.error(reject_reason)
+            entry.reject(reject_reason)
+            return False
 
-    serial_tree = BeautifulSoup(serial_html, 'html.parser')
-    table_node = serial_tree.find('table', class_=table_class_regexp)
-    if table_node:
-        href_regexp = re.compile(r'details.php\?id=(\d+)', flags=re.IGNORECASE)
-        link_nodes = table_node.find_all('a', href=href_regexp)
-        for link_node in link_nodes:
-            href = 'http://baibako.tv/' + link_node.get('href')
+        topic_id = url_match.group(1)
+        url = 'http://baibako.tv/download.php?id={0}'.format(topic_id)
+        entry['url'] = url
+        return True
 
-            href_match = href_regexp.search(href)
-            topic_id = href_match.group(1)
-            print("topic_id: {0}".format(topic_id))
+    def search(self, task, entry, config=None):
+        entries = set()
 
-            download_url = "http://baibako.tv/download.php?id={0}".format(topic_id)
-            print(download_url)
+        serials_url = 'http://baibako.tv/serials.php'
 
-            episode_title = link_node.text
+        log.debug("Fetching serials page `{0}`...".format(serials_url))
 
-            episode_match = episode_title_regexp.match(episode_title)
-            if episode_match:
-                ru_title = episode_match.group(1)
-                title = episode_match.group(2)
-                season = int(episode_match.group(3))
-                episode = int(episode_match.group(4))
-                quality = episode_match.group(5)
+        try:
+            serials_response = task.requests.get(serials_url)
+        except requests.RequestException as e:
+            log.error("Error while fetching page: {0}".format(e))
+            return None
+        serials_html = serials_response.text
 
-                print("{0} / {1} / s{2:02d}e{3:02d} / {4} - {5}".format(title, ru_title, season, episode, quality, href))
-    else:
-        print("table node not found")
+        shows = set()
+
+        serials_tree = BeautifulSoup(serials_html, 'html.parser')
+        serials_table_node = serials_tree.find('table', class_=table_class_regexp)
+        if not serials_table_node:
+            log.error('Error while parsing serials page: node <table class=`table.*`> are not found')
+            return None
+
+        serial_link_nodes = serials_table_node.find_all('a')
+        for serial_link_node in serial_link_nodes:
+            serial_title = serial_link_node.text
+            serial_link = serial_link_node.get('href')
+            serial_link = self.add_host_if_need(serial_link)
+
+            show = BaibakoShow([serial_title], serial_link)
+            shows.add(show)
+
+        log.debug("{0:d} show(s) are found".format(len(shows)))
+
+        serial_tab = self.config.get('serial_tab')
+        if not isinstance(serial_tab, str):
+            serial_tab = 'all'
+
+        search_regexp = re.compile(r'^(.*?)\s*s(\d+?)e(\d+?)$', flags=re.IGNORECASE)
+        episode_link_regexp = re.compile(r'details.php\?id=(\d+)', flags=re.IGNORECASE)
+
+        for search_string in entry.get('search_strings', [entry['title']]):
+            search_match = search_regexp.search(search_string)
+            if not search_match:
+                continue
+
+            search_title = search_match.group(1)
+            search_season = int(search_match.group(2))
+            search_episode = int(search_match.group(3))
+
+            for show in shows:
+                if search_title not in show.titles:
+                    continue
+
+                serial_url = show.url + '&tab=' + serial_tab
+                try:
+                    serial_response = task.requests.get(serial_url)
+                except requests.RequestException as e:
+                    log.error("Error while fetching page: {0}".format(e))
+                    continue
+                serial_html = serial_response.text
+
+                serial_tree = BeautifulSoup(serial_html, 'html.parser')
+                serial_table_node = serial_tree.find('table', class_=table_class_regexp)
+                if not serial_table_node:
+                    log.error('Error while parsing serial page: node <table class=`table.*`> are not found')
+                    continue
+
+                link_nodes = serial_table_node.find_all('a', href=episode_link_regexp)
+                for link_node in link_nodes:
+                    link_title = link_node.text
+                    episode_title_match = episode_title_regexp.search(link_title)
+                    if not episode_title_match:
+                        log.verbose("Error while parsing serial page: title `{0}` are not matched".format(link_title))
+                        continue
+
+                    season = int(episode_title_match.group(3))
+                    episode = int(episode_title_match.group(4))
+
+                    if season != search_season or episode != search_episode:
+                        continue
+
+                    ru_title = episode_title_match.group(1)
+                    title = episode_title_match.group(2)
+                    quality = episode_title_match.group(5)
+
+                    entry_title = "{0} / {1} / s{2:02d}e{3:02d} / {4}".format(title, ru_title, season, episode, quality)
+                    entry_url = link_node.get('href')
+                    entry_url = self.add_host_if_need(entry_url)
+
+                    entry = Entry()
+                    entry['title'] = entry_title
+                    entry['url'] = entry_url
+
+                    entries.add(entry)
+
+        return entries
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(BaibakoUrlRewrite, 'baibako', groups=['urlrewriter', 'search'], api_ver=2)
